f8d3f8e448e0f304c4e537e1f06b3970d2db0ffa
nikkosberces <nikkosberces@gmail.com>
2023-04-20 21:07:54 +0800

    first commit

---
+++ vite-project/main.js
@@ -0,0 +1,8 @@
+import "./style.css";
+import { add, subtract, multiply, divide } from "./scr/math";
+import calculate from "./scr/calculate";
+
+console.log(add(1, 2));
+console.log(subtract(1, 2));
+console.log(multiply(1, 2));
+console.log(divide(1, 2));
---
+++ vite-project/scr/module.js
@@ -0,0 +1,15 @@
+// export const PI = 3.14;
+
+// export function add(x, y) {
+//   return x + y;
+// }
+
+const PI = 3.14;
+
+function add(x, y) {
+  return x + y;
+}
+
+export { PI, add };
+
+// export default PI; // Single Default Value
